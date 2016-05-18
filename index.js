bc1e8292aa341b714094a07d55f2b84ac6bfa5be
Alex Aaron Pena <alexaaronpena@gmail.com>
2016-05-18 16:29:16 -0700

    hello...again

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === '<VERIFY_TOKEN>') {
+    if (req.query['hub.verify_token'] === '<HARVEY_BOT_DEVELOPMENT>') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')

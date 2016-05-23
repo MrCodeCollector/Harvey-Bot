d0617b060b2b5069d354f15061543f6720b2705e
Alex Aaron Pena <alexaaronpena@gmail.com>
2016-05-23 13:56:10 -0700

    Maybe now

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function sendWeatherMessage(sender) {
     }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: {access_token:process.env.PAGE_TOKEN},
         method: 'POST',
         json: {
             recipient: {id:sender},

c51d2193b005263e42a1308854288db0327deae8
Alex Aaron Pena <alexaaronpena@gmail.com>
2016-05-25 07:59:22 -0700

    fixed now maybe

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.listen((process.env.PORT || 3000));
-
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -21,7 +19,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    if (req.query['hub.verify_token'] === 'harvey_bot_development') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token, woops')
@@ -32,91 +30,38 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 
-// app.post('/webhook/', function (req, res) {
-//     messaging_events = req.body.entry[0].messaging
-//     for (i = 0; i < messaging_events.length; i++) {
-//         event = req.body.entry[0].messaging[i]
-//         sender = event.sender.id
-//         if (event.message && event.message.text) {
-//             text = event.message.text
-//             if(text === 'How\'s the Weather?') {
-//               sendWeatherMessage(sender)
-//               continue
-//             }
-//             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-//         }
-//     }
-//     res.sendStatus(200)
-// })
-//
-// var token = 'EAAQtPmyLcRwBAJFhGdP1wzFtTYjZBBZAT3fIUJboiuQmTNidpBcnPOPH5FVHDyZCtZAzRMiR6PCDhg8C2CEmOWjT6B2jRxljafwenZCoRWwE4fB107mboR8EfUVHKwlyI9GTOhDIsZBfFMAb7ZBh6McxzbsHOUHidhmVQ34kBVT6gZDZD'
-//
-// function sendWeatherMessage(sender) {
-//   messageData = {
-//         "attachment": {
-//             "type": "template",
-//             "payload": {
-//                 "template_type": "generic",
-//                 "elements": [{
-//                     "title": "Weather Forecast",
-//                     "subtitle": "Weather type",
-//                     "image_url": "http://phandroid.s3.amazonaws.com/wp-content/uploads/2014/04/yahoo.png",
-//                     "buttons": [{
-//                         "type": "web_url",
-//                         "url": "https://weather.com",
-//                         "title": "Weather Report"
-//                     }, {
-//                         "type": "postback",
-//                         "title": "Postback",
-//                         "payload": "Payload for first element in a generic bubble",
-//                     }],
-//                 }, {
-//                     "title": "Second card",
-//                     "subtitle": "possible other stuff",
-//                     "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-//                     "buttons": [{
-//                         "type": "postback",
-//                         "title": "Postback",
-//                         "payload": "Payload for second element in a generic bubble",
-//                     }],
-//                 }]
-//             }
-//         }
-//     }
-//     request({
-//         url: 'https://graph.facebook.com/v2.6/me/messages',
-//         qs: {access_token:token},
-//         method: 'POST',
-//         json: {
-//             recipient: {id:sender},
-//             message: messageData,
-//         }
-//     }, function(error, response, body) {
-//         if (error) {
-//             console.log('Error sending messages: ', error)
-//         } else if (response.body.error) {
-//             console.log('Error: ', response.body.error)
-//         }
-//     })
-// }
-//
-// function sendTextMessage(sender, text) {
-//     messageData = {
-//         text:text
-//     }
-//     request({
-//         url: 'https://graph.facebook.com/v2.6/me/messages',
-//         qs: {access_token:token},
-//         method: 'POST',
-//         json: {
-//             recipient: {id:sender},
-//             message: messageData,
-//         }
-//     }, function(error, response, body) {
-//         if (error) {
-//             console.log('Error sending messages: ', error)
-//         } else if (response.body.error) {
-//             console.log('Error: ', response.body.error)
-//         }
-//     })
-// }
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "EAAQtPmyLcRwBABDJORRklIWNOMgFNqQphGyuASod6kuyZBeYHsgi6vBLj449wbZCrFJZAZAIqWiMJgQwNWpWiirNZAPgFGywsusnY3kMOPDZB9pMAnb5lW7eWvmDjPm19lxtzRzbZCXoy5VFRbuUmXJ1QCJAqZAUUZB7u4R3OBGVQsgZDZD"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}

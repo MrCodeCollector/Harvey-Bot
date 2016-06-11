af73a298738979f1bf3da5fb113c54d99a192026
Alex Aaron Pena <alexaaronpena@gmail.com>
2016-06-11 14:44:18 -0700

    testing out wit.ai

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
 
+var Wit = require('node-wit').Wit;
+var client = new Wit(P4OHFVSITFMWAVO5VIPLFMC77DDTKEPI, actions);
+var context = {};
+
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -36,6 +40,18 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id
         if (event.message && event.message.text) {
             text = event.message.text
+
+
+            client.message(text, context, (error, data) => {
+              if (error) {
+                console.log('Oops! Got an error: ' + error);
+              } else {
+                witContacted(sender)
+                continue
+              }
+            });
+
+
             if(text === 'What\'s the weather like?') {
               sendWeatherMessage(sender)
               continue
@@ -52,6 +68,27 @@ app.post('/webhook/', function (req, res) {
 })
 
 var token = "EAAQtPmyLcRwBAFEfYjJNeTVPdY3sMsWm5dbSBFLMYsTwpAdMHRbefaI46qGPmU8D4TuwChI29BmYhX2wWqSI3EBdDIhZAXhhuZCWzBot0nD15S7KRZAHIjYrZCGuM31C7CQ3lzhcIF4ye1JmhEVZB6KzZA9QZBYAiZAxXFgnbGMWTAZDZD"
+
+function witContacted(sender, text) {
+    messageData = {
+        text:'It\'s working!'
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 function sendTextMessage(sender, text) {
     messageData = {
